c55e4339f463c7f5d76612d0834914280faeeb92
Manasvini-khoros <manasviniarul@gmail.com>
2020-11-11 16:20:42 +0530

    first commit

---
+++ src/demo.java
@@ -0,0 +1,22 @@
+package src;
+
+public class demo {
+
+    public static void main(String args[]) {
+
+        System.out.println("demooo");
+
+        System.out.println("hiiii");
+
+        System.out.println("tttttt");
+
+        System.out.println("ssssssss");
+
+        System.out.println("oooooo");
+
+        System.out.println("ggggggg");
+
+        System.out.println("uuuuu");
+
+    }
+}
